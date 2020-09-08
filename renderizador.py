46e0289dc0413c73c1fc8eb6078c28bb75c13a92
RogerPina2 <roger.pina12@hotmail.com>
2020-09-08 17:49:05 -0300

    Descrição das funções

--- renderizador.py
+++ renderizador.py
@@ -9,9 +9,9 @@ import gpu          # Simula os recursos de uma GPU
        
 def polypoint2D(point, color):
     """ Função usada para renderizar Polypoint2D. """
-    # gpu.GPU.set_pixel(3, 1, 255, 0, 0) # altera um pixel da imagem
-    # cuidado com as cores, o X3D especifica de (0,1) e o Framebuffer de (0,255)
 
+    # O laço passa por todos elementos da lista point
+    # pega a posição de cada ponto e pinta o píxel correspondente 
     i = 0
     while (i < len(point)):
         gpu.GPU.set_pixel(int(point[i]), int(point[i+1]), color[0]*255, color[1]*255, color[2]*255)
@@ -33,6 +33,7 @@ def polyline2D(lineSegments, color):
 
 
     s = (y1-y0)/(x1-x0) # Inclinação da reta
+    
     x, y = x0, y0
     while x <= x1:
         if s > 1:
@@ -49,28 +50,29 @@ def polyline2D(lineSegments, color):
         y += s
         x += 1
 
-def L(x, y, x0, y0, x1, y1):
+def eqDaReta(x, y, x0, y0, x1, y1):
+    """ Função usada para calcular a Equação da Reta"""
     dx = x1 - x0
     dy = y1 - y0
 
     if (x - x0)*dy - (y - y0)*dx >= 0:
         return True
-    else:
-        return False
+    
+    return False
 
 def inside(x, y, vertices):
+    "Função usada para verificar se um píxel esta dentro do triangulo"
     x0, y0 = vertices[0], vertices[1]
     x1, y1 = vertices[2], vertices[3]
     x2, y2 = vertices[4], vertices[5]
 
-    if L(x, y, x0, y0, x1, y1) and L(x, y, x1, y1, x2, y2) and L(x, y, x2, y2, x0, y0):
+    if eqDaReta(x, y, x0, y0, x1, y1) and eqDaReta(x, y, x1, y1, x2, y2) and eqDaReta(x, y, x2, y2, x0, y0):
         return True
-    else:
-        return False
+    
+    return False
 
 def triangleSet2D(vertices, color):
     """ Função usada para renderizar TriangleSet2D. """
-    #gpu.GPU.set_pixel(24, 8, 255, 255, 0) # altera um pixel da imagem
     
     xmax, ymax = 30, 20
 
@@ -99,7 +101,7 @@ if __name__ == '__main__':
     # Valores padrão da aplicação
     width = LARGURA
     height = ALTURA
-    x3d_file = "exemplo2.x3d"
+    x3d_file = "exemplo3.x3d"
     image_file = "tela.png"
 
     # Tratando entrada de parâmetro
