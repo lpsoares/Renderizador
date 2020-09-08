5baf2ae936d4d866365dc03f86fb81855f41e443
RogerPina2 <roger.pina12@hotmail.com>
2020-09-07 21:47:55 -0300

    triangleSet e Supersampling

--- renderizador.py
+++ renderizador.py
@@ -19,11 +19,6 @@ def polypoint2D(point, color):
 
 def polyline2D(lineSegments, color):
     """ Função usada para renderizar Polyline2D. """
-    #x = gpu.GPU.width//2
-    #y = gpu.GPU.height//2
-    #gpu.GPU.set_pixel(x, y, 255, 0, 0) # altera um pixel da imagem
-
-    print(lineSegments)
 
     if lineSegments[0] <= lineSegments[2]:
         x0 = lineSegments[0] 
@@ -38,27 +33,63 @@ def polyline2D(lineSegments, color):
 
 
     s = (y1-y0)/(x1-x0) # Inclinação da reta
-    x = x0
-    y = y0
+    x, y = x0, y0
     while x <= x1:
         if s > 1:
             k = y
             for i in range(int(y+s) - int(y)):
-                gpu.GPU.set_pixel(int(x), int(k+i), 255, 0, 255) # altera um pixel da imagem
+                gpu.GPU.set_pixel(int(x), int(k+i), color[0]*255, color[1]*255, color[2]*255) # altera um pixel da imagem
         elif s < -1:
             k = y
-            
             for i in range(abs(int(y) - int(y+s))):
-                gpu.GPU.set_pixel(int(x), int(k-i), 255, 0, 255) # altera um pixel da imagem
+                gpu.GPU.set_pixel(int(x), int(k-i), color[0]*255, color[1]*255, color[2]*255) # altera um pixel da imagem
         else:
-            gpu.GPU.set_pixel(int(x), int(y), 255, 0, 255) # altera um pixel da imagem
+            gpu.GPU.set_pixel(int(x), int(y), color[0]*255, color[1]*255, color[2]*255) # altera um pixel da imagem
       
         y += s
         x += 1
 
+def L(x, y, x0, y0, x1, y1):
+    dx = x1 - x0
+    dy = y1 - y0
+
+    if (x - x0)*dy - (y - y0)*dx >= 0:
+        return True
+    else:
+        return False
+
+def inside(x, y, vertices):
+    x0, y0 = vertices[0], vertices[1]
+    x1, y1 = vertices[2], vertices[3]
+    x2, y2 = vertices[4], vertices[5]
+
+    if L(x, y, x0, y0, x1, y1) and L(x, y, x1, y1, x2, y2) and L(x, y, x2, y2, x0, y0):
+        return True
+    else:
+        return False
+
 def triangleSet2D(vertices, color):
     """ Função usada para renderizar TriangleSet2D. """
-    gpu.GPU.set_pixel(24, 8, 255, 255, 0) # altera um pixel da imagem
+    #gpu.GPU.set_pixel(24, 8, 255, 255, 0) # altera um pixel da imagem
+    
+    xmax, ymax = 30, 20
+
+    for x in range(xmax):
+        for y in range(ymax):
+            supersampling = 4
+            mean = 0
+            
+            if inside(x+0.25, y+0.25, vertices):
+                mean += 1/supersampling
+            if inside(x+0.25, y+0.75, vertices):
+                mean += 1/supersampling
+            if inside(x+0.75, y+0.25, vertices):
+                mean += 1/supersampling
+            if inside(x+0.75, y+0.75, vertices):
+                mean += 1/supersampling
+
+            if mean > 0:
+                gpu.GPU.set_pixel(x, y, color[0]*255*mean, color[1]*255*mean, color[2]*255*mean)
 
 LARGURA = 30
 ALTURA = 20
