efc121257cd82685c0215e8c760b31008ca583c1
sabrina <sbrnmchd@gmail.com>
2021-08-26 23:06:19 -0300

    Salve

--- renderizador/rotinas.py
+++ renderizador/rotinas.py
@@ -9,11 +9,16 @@ Disciplina: Computação Gráfica
 Data:
 """
 
-import gpu          # Simula os recursos de uma GPU
+import gpu  # Simula os recursos de uma GPU
+
 
 # web3d.org/documents/specifications/19775-1/V3.0/Part01/components/geometry2D.html#Polypoint2D
 def polypoint2D(point, colors):
     """Função usada para renderizar Polypoint2D."""
+    for i in range(0, int(len(point)), 2):
+        gpu.GPU.set_pixel(int(point[i]), int(point[i + 1]), 255 * colors['emissiveColor'][0],
+                          255 * colors['emissiveColor'][1], 255 * colors['emissiveColor'][2])
+
     # Nessa função você receberá pontos no parâmetro point, esses pontos são uma lista
     # de pontos x, y sempre na ordem. Assim point[0] é o valor da coordenada x do
     # primeiro ponto, point[1] o valor y do primeiro ponto. Já point[2] é a
@@ -22,16 +27,54 @@ def polypoint2D(point, colors):
     # O parâmetro colors é um dicionário com os tipos cores possíveis, para o Polypoint2D
     # você pode assumir o desenho dos pontos com a cor emissiva (emissiveColor).
 
-    # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-    print("Polypoint2D : pontos = {0}".format(point)) # imprime no terminal pontos
-    print("Polypoint2D : colors = {0}".format(colors)) # imprime no terminal as cores
-    # Exemplo:
-    gpu.GPU.set_pixel(3, 1, 255, 0, 0) # altera um pixel da imagem (u, v, r, g, b)
     # cuidado com as cores, o X3D especifica de (0,1) e o Framebuffer de (0,255)
+
 
 # web3d.org/documents/specifications/19775-1/V3.0/Part01/components/geometry2D.html#Polyline2D
 def polyline2D(lineSegments, colors):
-    """Função usada para renderizar Polyline2D."""
+    # """Função usada para renderizar Polyline2D."""
+    for i in range(0, int(len(lineSegments)), 4):
+        x_1 = lineSegments[i]
+        y_1 = lineSegments[i + 1]
+        x_2 = lineSegments[i + 2]
+        y_2 = lineSegments[i + 3]
+        u = x_1
+        v = y_1
+        s = ((y_2 - y_1) / (x_2 - x_1))
+        p = 0
+        if s > 1:
+            p = 1/s
+        elif s < -1:
+            p = -1/s
+        else:
+            p = 1
+        if x_2 > x_1:
+            while x_2 + p > u:
+                gpu.GPU.set_pixel(int(u), int(v), 255/2 * colors['emissiveColor'][0],
+                          255/2 * colors['emissiveColor'][1], 255/2 * colors['emissiveColor'][2])
+                if s > 1:
+                    u = u + 1 / s
+                    v = v + 1
+                elif s < -1:
+                    u = u - 1 / s
+                    v = v - 1
+                else:
+                    u = u + 1
+                    v = v + s
+        else:
+            while x_2 - p < u:
+                gpu.GPU.set_pixel(int(u), int(v), 255/2 * colors['emissiveColor'][0],
+                          255/2 * colors['emissiveColor'][1], 255/2 * colors['emissiveColor'][2])
+                if s > 1:
+                    u = u - 1 / s
+                    v = v - 1
+                elif s < -1:
+                    u = u + 1 / s
+                    v = v + 1
+                else:
+                    u = u - 1
+                    v = v - s
+
     # Nessa função você receberá os pontos de uma linha no parâmetro lineSegments, esses
     # pontos são uma lista de pontos x, y sempre na ordem. Assim point[0] é o valor da
     # coordenada x do primeiro ponto, point[1] o valor y do primeiro ponto. Já point[2] é
@@ -42,16 +85,60 @@ def polyline2D(lineSegments, colors):
     # O parâmetro colors é um dicionário com os tipos cores possíveis, para o Polyline2D
     # você pode assumir o desenho das linhas com a cor emissiva (emissiveColor).
 
-    print("Polyline2D : lineSegments = {0}".format(lineSegments)) # imprime no terminal
-    print("Polyline2D : colors = {0}".format(colors)) # imprime no terminal as cores
-    # Exemplo:
-    pos_x = gpu.GPU.width//2
-    pos_y = gpu.GPU.height//2
-    gpu.GPU.set_pixel(pos_x, pos_y, 255, 0, 0) # altera um pixel da imagem (u, v, r, g, b)
+
+def dot(x_A, y_A, x_B, y_B, x_ponto, y_ponto):
+    return (y_B - y_A)*x_ponto - (x_B - x_A)*y_ponto + (x_B - x_A)*y_A - (y_B - y_A)*x_A
+
+
+def inside(x_A, y_A, x_B, y_B, x_C, y_C, x_ponto, y_ponto):
+    d1 = dot(x_A, y_A, x_B, y_B, x_ponto, y_ponto)
+    d2 = dot(x_B, y_B, x_C, y_C, x_ponto, y_ponto)
+    d3 = dot(x_C, y_C, x_A, y_A, x_ponto, y_ponto)
+    if dot(x_A, y_A, x_B, y_B, x_ponto, y_ponto) >= 0 and dot(x_B, y_B, x_C, y_C, x_ponto, y_ponto) >= 0 and dot(x_C, y_C, x_A, y_A, x_ponto, y_ponto) >= 0:
+        return True
+    else:
+        return False
+
 
 # web3d.org/documents/specifications/19775-1/V3.0/Part01/components/geometry2D.html#TriangleSet2D
 def triangleSet2D(vertices, colors):
     """Função usada para renderizar TriangleSet2D."""
+    for i in range(0, len(vertices), 6):
+        x_1 = vertices[i]
+        y_1 = vertices[i + 1]
+        x_2 = vertices[i + 2]
+        y_2 = vertices[i + 3]
+        x_3 = vertices[i + 4]
+        y_3 = vertices[i + 5]
+        minX = x_1
+        maxX = x_1
+        minY = y_1
+        maxY = y_1
+        if x_2 > maxX:
+            maxX = x_2
+        if x_3 > maxX:
+            maxX = x_3
+        if y_2 > maxY:
+            maxY = y_2
+        if y_3 > maxY:
+            maxY = y_3
+        if x_2 < minX:
+            minX = x_2
+        if x_3 < minX:
+            minX = x_3
+        if y_2 < minY:
+            minY = y_2
+        if y_3 < minY:
+            minY = y_3
+
+        for x in range(int(minX)*2, int(maxX)*2):
+            for y in range(int(minY)*2, int(maxY)*2):
+                if inside(x_1, y_1, x_2, y_2, x_3, y_3, x/2, y/2):
+                    gpu.GPU.set_pixel(int(x/2), int(y/2), 255 / 2 * colors['emissiveColor'][0],
+                                      255 / 2 * colors['emissiveColor'][1], 255 / 2 * colors['emissiveColor'][2])
+
+
+
     # Nessa função você receberá os vertices de um triângulo no parâmetro vertices,
     # esses pontos são uma lista de pontos x, y sempre na ordem. Assim point[0] é o
     # valor da coordenada x do primeiro ponto, point[1] o valor y do primeiro ponto.
@@ -59,10 +146,11 @@ def triangleSet2D(vertices, colors):
     # quantidade de pontos é sempre multiplo de 3, ou seja, 6 valores ou 12 valores, etc.
     # O parâmetro colors é um dicionário com os tipos cores possíveis, para o TriangleSet2D
     # você pode assumir o desenho das linhas com a cor emissiva (emissiveColor).
-    print("TriangleSet2D : vertices = {0}".format(vertices)) # imprime no terminal
-    print("TriangleSet2D : colors = {0}".format(colors)) # imprime no terminal as cores
+    print("TriangleSet2D : vertices = {0}".format(vertices))  # imprime no terminal
+    print("TriangleSet2D : colors = {0}".format(colors))  # imprime no terminal as cores
     # Exemplo:
-    gpu.GPU.set_pixel(24, 8, 255, 255, 0) # altera um pixel da imagem (u, v, r, g, b)
+    gpu.GPU.set_pixel(24, 8, 255, 255, 0)  # altera um pixel da imagem (u, v, r, g, b)
+
 
 def triangleSet(point, colors):
     """Função usada para renderizar TriangleSet."""
@@ -78,8 +166,9 @@ def triangleSet(point, colors):
     # você pode assumir o desenho das linhas com a cor emissiva (emissiveColor).
 
     # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-    print("TriangleSet : pontos = {0}".format(point)) # imprime no terminal pontos
-    print("TriangleSet : colors = {0}".format(colors)) # imprime no terminal as cores
+    print("TriangleSet : pontos = {0}".format(point))  # imprime no terminal pontos
+    print("TriangleSet : colors = {0}".format(colors))  # imprime no terminal as cores
+
 
 def viewpoint(position, orientation, fieldOfView):
     """Função usada para renderizar (na verdade coletar os dados) de Viewpoint."""
@@ -92,6 +181,7 @@ def viewpoint(position, orientation, fieldOfView):
     print("position = {0} ".format(position), end='')
     print("orientation = {0} ".format(orientation), end='')
     print("fieldOfView = {0} ".format(fieldOfView))
+
 
 def transform_in(translation, scale, rotation):
     """Função usada para renderizar (na verdade coletar os dados) de Transform."""
@@ -106,12 +196,13 @@ def transform_in(translation, scale, rotation):
     # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
     print("Transform : ", end='')
     if translation:
-        print("translation = {0} ".format(translation), end='') # imprime no terminal
+        print("translation = {0} ".format(translation), end='')  # imprime no terminal
     if scale:
-        print("scale = {0} ".format(scale), end='') # imprime no terminal
+        print("scale = {0} ".format(scale), end='')  # imprime no terminal
     if rotation:
-        print("rotation = {0} ".format(rotation), end='') # imprime no terminal
+        print("rotation = {0} ".format(rotation), end='')  # imprime no terminal
     print("")
+
 
 def transform_out():
     """Função usada para renderizar (na verdade coletar os dados) de Transform."""
@@ -122,6 +213,7 @@ def transform_out():
 
     # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
     print("Saindo de Transform")
+
 
 def triangleStripSet(point, stripCount, colors):
     """Função usada para renderizar TriangleStripSet."""
@@ -138,7 +230,8 @@ def triangleStripSet(point, stripCount, colors):
     for i, strip in enumerate(stripCount):
         print("strip[{0}] = {1} ".format(i, strip), end='')
     print("")
-    print("TriangleStripSet : colors = {0}".format(colors)) # imprime no terminal as cores
+    print("TriangleStripSet : colors = {0}".format(colors))  # imprime no terminal as cores
+
 
 def indexedTriangleStripSet(point, index, colors):
     """Função usada para renderizar IndexedTriangleStripSet."""
@@ -155,7 +248,8 @@ def indexedTriangleStripSet(point, index, colors):
 
     # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
     print("IndexedTriangleStripSet : pontos = {0}, index = {1}".format(point, index))
-    print("IndexedTriangleStripSet : colors = {0}".format(colors)) # imprime no terminal as cores
+    print("IndexedTriangleStripSet : colors = {0}".format(colors))  # imprime no terminal as cores
+
 
 def box(size, colors):
     """Função usada para renderizar Boxes."""
@@ -167,8 +261,8 @@ def box(size, colors):
     # encontre os vértices e defina os triângulos.
 
     # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-    print("Box : size = {0}".format(size)) # imprime no terminal pontos
-    print("Box : colors = {0}".format(colors)) # imprime no terminal as cores
+    print("Box : size = {0}".format(size))  # imprime no terminal pontos
+    print("Box : colors = {0}".format(colors))  # imprime no terminal as cores
 
 
 def indexedFaceSet(coord, coordIndex, colorPerVertex, color, colorIndex,
@@ -204,4 +298,4 @@ def indexedFaceSet(coord, coordIndex, colorPerVertex, color, colorIndex,
     if current_texture:
         image = gpu.GPU.load_texture(current_texture[0])
         print("\t Matriz com image = {0}".format(image))
-    print("IndexedFaceSet : colors = {0}".format(colors)) # imprime no terminal as cores
+    print("IndexedFaceSet : colors = {0}".format(colors))  # imprime no terminal as cores
