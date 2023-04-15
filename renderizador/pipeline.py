a747d34f228da80b8b4500f8105f8186ef149702
Luciano Pereira Soares <lpsoares@gmail.com>
2023-04-15 11:56:22 -0300

    iniciando sistema de pipeline

---
+++ renderizador/pipeline.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+Sistema para pipeline gráfico.
+
+Desenvolvido por: Luciano Soares <lpsoares@insper.edu.br>
+Disciplina: Computação Gráfica
+Data: 15 de Abril de 2023
+"""
+
--- renderizador/scenegraph.py
+++ renderizador/scenegraph.py
@@ -1,16 +1,27 @@
-#from treelib import Node, Tree
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+Base para listar o grafo de cena no terminal do usuário.
+
+Desenvolvido por: Luciano Soares <lpsoares@insper.edu.br>
+Disciplina: Computação Gráfica
+Data: 15 de Abril de 2023
+"""
+
+from treelib import Node, Tree
 
 # Desenha o Grafo de Cena no console
 class Graph:
-    # def parse(self, node):
-    #     for child in node:
-    #         tag = child.tag + (" "+str(child.attrib) if child.attrib else "")
-    #         self.tree.create_node(tag, child.__hash__(), node.__hash__())
-    #         self.parse(child)
+    def parse(self, node):
+        for child in node:
+            tag = child.tag + (" "+str(child.attrib) if child.attrib else "")
+            self.tree.create_node(tag, child.__hash__(), node.__hash__())
+            self.parse(child)
 
     def __init__(self, node):
-        # self.tree = Tree()
-        # self.tree.create_node(node.tag, node.__hash__())
-        # self.parse(node)
-        # self.tree.show()
+        self.tree = Tree()
+        self.tree.create_node(node.tag, node.__hash__())
+        self.parse(node)
+        self.tree.show()
         pass
