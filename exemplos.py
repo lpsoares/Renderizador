f539b6474521add5b787d878ecb78bc2fbdf047b
Luciano Pereira Soares <lpsoares@gmail.com>
2024-09-05 22:08:07 -0300

    melhor organizando os exemplos x3d

--- exemplos.py
+++ exemplos.py
@@ -30,53 +30,51 @@ signal.signal(signal.SIGINT, signal_handler)
 
 TESTE = []
 
-# Exemplos 2D
-# Rasterização
-TESTE.append(["pontos", "-i", DIR+"2D/pontos/pontos.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["linhas", "-i", DIR+"2D/linhas/linhas.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["octogono", "-i", DIR+"2D/linhas/octogono.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["linhas_fora", "-i", DIR+"2D/linhas/linhas_fora.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["var_lin", "-i", DIR+"2D/linhas/varias_linhas.x3d", "-w", "600", "-h", "400", "-p"])
-TESTE.append(["circulo", "-i", DIR+"2D/linhas/circulo.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["tris_2D", "-i", DIR+"2D/triangulos/triangulos.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["helice", "-i", DIR+"2D/triangulos/helice.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["tri_alta", "-i", DIR+"2D/triangulos/triangulos_alta.x3d", "-w", "600", "-h", "400", "-p"])
+# Rasterização 2D
+TESTE.append(["aleatorios", "-i", DIR+"2D/pontos/aleatorios/aleatorios.x3d", "-w", "30", "-h", "20", "-p"])
+TESTE.append(["linhas_cores", "-i", DIR+"2D/linhas/linhas_cores/linhas_cores.x3d", "-w", "30", "-h", "20", "-p"])
+TESTE.append(["octogono", "-i", DIR+"2D/linhas/octogono/octogono.x3d", "-w", "30", "-h", "20", "-p"])
+TESTE.append(["linhas_cruzes", "-i", DIR+"2D/linhas/linhas_cruzes/linhas_cruzes.x3d", "-w", "30", "-h", "20", "-p"])
+TESTE.append(["varias_linhas", "-i", DIR+"2D/linhas/varias_linhas/varias_linhas.x3d", "-w", "600", "-h", "400", "-p"])
+TESTE.append(["circulo", "-i", DIR+"2D/linhas/circulo/circulo.x3d", "-w", "30", "-h", "20", "-p"])
+TESTE.append(["triangulos", "-i", DIR+"2D/triangulos/triangulos/triangulos.x3d", "-w", "30", "-h", "20", "-p"])
+TESTE.append(["helice", "-i", DIR+"2D/triangulos/helice/helice.x3d", "-w", "30", "-h", "20", "-p"])
+TESTE.append(["pontas", "-i", DIR+"2D/triangulos/pontas/pontas.x3d", "-w", "600", "-h", "400", "-p"])
 
-# Exemplos 3D
-# Visualização 3D sem shading
-TESTE.append(["um_tri", "-i", DIR+"3D/triangulos/um_triangulo.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["var_tri", "-i", DIR+"3D/triangulos/varios_triangulos.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["zoom", "-i", DIR+"3D/triangulos/zoom.x3d", "-w", "300", "-h", "200", "-p"])
+# Triângulos em 3D
+TESTE.append(["um_triangulo", "-i", DIR+"3D/malhas/um_triangulo/um_triangulo.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["varios_triangs", "-i", DIR+"3D/malhas/varios_triangs/varios_triangs.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["zoom", "-i", DIR+"3D/malhas/zoom/zoom.x3d", "-w", "300", "-h", "200", "-p"])
 
-# Geometrias 3D
-TESTE.append(["tira_tri", "-i", DIR+"3D/triangulos/tiratrig.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["letras", "-i", DIR+"3D/cores/letras.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["avatar", "-i", DIR+"3D/avatar/avatar.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["leques", "-i", DIR+"3D/cores/leques.x3d", "-w", "480", "-h", "320", "-p"])
+# Malhas 3D
+TESTE.append(["tiras", "-i", DIR+"3D/malhas/tiras/tiras.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["letras", "-i", DIR+"3D/malhas/letras/letras.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["avatar", "-i", DIR+"3D/grafo_de_cena/avatar/avatar.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["leques", "-i", DIR+"3D/malhas/leques/leques.x3d", "-w", "480", "-h", "320", "-p"])
+
+# Primitivas
+TESTE.append(["duas", "-i", DIR+"3D/primitivas/duas/duas.x3d", "-w", "300", "-h", "200", "-p"])
 
 # Z-buffer e Transparência
-TESTE.append(["retang", "-i", DIR+"3D/retangulos/retangulos.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["transp", "-i", DIR+"3D/transparencia/transparencia.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["retangulos", "-i", DIR+"3D/malhas/retangulos/retangulos.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["transparente", "-i", DIR+"3D/transparencia/transparente/transparente.x3d", "-w", "300", "-h", "200", "-p"])
 
 # Interpolações de Cores
-TESTE.append(["cores", "-i", DIR+"3D/cores/cores.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["flechas", "-i", DIR+"3D/cores/flechas.x3d", "-w", "480", "-h", "320", "-p"])
+TESTE.append(["quadrado", "-i", DIR+"3D/cores/quadrado/quadrado.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["flechas", "-i", DIR+"3D/cores/flechas/flechas.x3d", "-w", "480", "-h", "320", "-p"])
 
 # Texturas
-TESTE.append(["textura", "-i", DIR+"3D/texturas/textura.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["texturas", "-i", DIR+"3D/texturas/texturas.x3d", "-w", "300", "-h", "200", "-p"])
-
-# Primitivas 3D
-TESTE.append(["primitivas", "-i", DIR+"3D/box/primitivas.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["textura", "-i", DIR+"3D/texturas/textura/textura.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["texturas", "-i", DIR+"3D/texturas/texturas/texturas.x3d", "-w", "300", "-h", "200", "-p"])
 
 # Iluminação
-TESTE.append(["tri_3D", "-i", DIR+"3D/triangulos/triang3d.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["caixas", "-i", DIR+"3D/box/box.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["esferas", "-i", DIR+"3D/iluminacao/esferas.x3d", "-w", "180", "-h", "120", "-p"])
+TESTE.append(["difusos", "-i", DIR+"3D/malhas/difusos/difusos.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["caixas", "-i", DIR+"3D/primitivas/caixas/caixas.x3d", "-w", "300", "-h", "200", "-p"])
+TESTE.append(["esferas", "-i", DIR+"3D/iluminacao/esferas/esferas.x3d", "-w", "180", "-h", "120", "-p"])
 
 # Animações
-TESTE.append(["onda", "-i", DIR+"3D/animacoes/onda.x3d", "-w", "300", "-h", "200"])
-TESTE.append(["piramide", "-i", DIR+"3D/animacoes/piramide.x3d", "-w", "300", "-h", "200"])
+TESTE.append(["onda", "-i", DIR+"3D/animacoes/onda/onda.x3d", "-w", "300", "-h", "200"])
+TESTE.append(["piramide", "-i", DIR+"3D/animacoes/piramide/piramide.x3d", "-w", "300", "-h", "200"])
 
 # Lista os exemplos registrados (em 3 colunas)
 colunas = 4
