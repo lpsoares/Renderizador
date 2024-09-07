c0585b228625ecc33279525c2e450cac746ebd05
Luciano Pereira Soares <lpsoares@gmail.com>
2024-09-06 21:45:03 -0300

    usando um json para organizar os exemplos

--- exemplos.py
+++ exemplos.py
@@ -9,10 +9,12 @@ Disciplina: Computação Gráfica
 Data: 17 de Agosto de 2021
 """
 
-import sys
 import subprocess
-import signal
 import time
+import json
+import os
+import signal
+import sys
 
 DIR = "docs/exemplos/"
 
@@ -30,51 +32,20 @@ signal.signal(signal.SIGINT, signal_handler)
 
 TESTE = []
 
-# Rasterização 2D
-TESTE.append(["aleatorios", "-i", DIR+"2D/pontos/aleatorios/aleatorios.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["linhas_cores", "-i", DIR+"2D/linhas/linhas_cores/linhas_cores.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["octogono", "-i", DIR+"2D/linhas/octogono/octogono.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["linhas_cruzes", "-i", DIR+"2D/linhas/linhas_cruzes/linhas_cruzes.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["varias_linhas", "-i", DIR+"2D/linhas/varias_linhas/varias_linhas.x3d", "-w", "600", "-h", "400", "-p"])
-TESTE.append(["circulo", "-i", DIR+"2D/linhas/circulo/circulo.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["triangulos", "-i", DIR+"2D/triangulos/triangulos/triangulos.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["helice", "-i", DIR+"2D/triangulos/helice/helice.x3d", "-w", "30", "-h", "20", "-p"])
-TESTE.append(["pontas", "-i", DIR+"2D/triangulos/pontas/pontas.x3d", "-w", "600", "-h", "400", "-p"])
+# Load the JSON data
+with open('docs/examples.json', 'r') as f:
+    data = json.load(f)
 
-# Triângulos em 3D
-TESTE.append(["um_triangulo", "-i", DIR+"3D/malhas/um_triangulo/um_triangulo.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["varios_triangs", "-i", DIR+"3D/malhas/varios_triangs/varios_triangs.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["zoom", "-i", DIR+"3D/malhas/zoom/zoom.x3d", "-w", "300", "-h", "200", "-p"])
+# Populate the TESTE list based on the JSON data
+for section in data['examples']:
+    for example in section['examples']:
+        path = example['path']
+        x3d = example['x3d']
+        w = str(example.get('width', 640))
+        h = str(example.get('height', 480))
+        p = example.get('pause', False)
+        TESTE.append([x3d, "-i", os.path.join(DIR, path, f"{x3d}/{x3d}.x3d"), "-w", w, "-h", h]  + (["-p"] if p else []))
 
-# Malhas 3D
-TESTE.append(["tiras", "-i", DIR+"3D/malhas/tiras/tiras.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["letras", "-i", DIR+"3D/malhas/letras/letras.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["avatar", "-i", DIR+"3D/grafo_de_cena/avatar/avatar.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["leques", "-i", DIR+"3D/malhas/leques/leques.x3d", "-w", "480", "-h", "320", "-p"])
-
-# Primitivas
-TESTE.append(["duas", "-i", DIR+"3D/primitivas/duas/duas.x3d", "-w", "300", "-h", "200", "-p"])
-
-# Z-buffer e Transparência
-TESTE.append(["retangulos", "-i", DIR+"3D/malhas/retangulos/retangulos.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["transparente", "-i", DIR+"3D/transparencia/transparente/transparente.x3d", "-w", "300", "-h", "200", "-p"])
-
-# Interpolações de Cores
-TESTE.append(["quadrado", "-i", DIR+"3D/cores/quadrado/quadrado.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["flechas", "-i", DIR+"3D/cores/flechas/flechas.x3d", "-w", "480", "-h", "320", "-p"])
-
-# Texturas
-TESTE.append(["textura", "-i", DIR+"3D/texturas/textura/textura.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["texturas", "-i", DIR+"3D/texturas/texturas/texturas.x3d", "-w", "300", "-h", "200", "-p"])
-
-# Iluminação
-TESTE.append(["difusos", "-i", DIR+"3D/malhas/difusos/difusos.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["caixas", "-i", DIR+"3D/primitivas/caixas/caixas.x3d", "-w", "300", "-h", "200", "-p"])
-TESTE.append(["esferas", "-i", DIR+"3D/iluminacao/esferas/esferas.x3d", "-w", "180", "-h", "120", "-p"])
-
-# Animações
-TESTE.append(["onda", "-i", DIR+"3D/animacoes/onda/onda.x3d", "-w", "300", "-h", "200"])
-TESTE.append(["piramide", "-i", DIR+"3D/animacoes/piramide/piramide.x3d", "-w", "300", "-h", "200"])
 
 # Lista os exemplos registrados (em 3 colunas)
 colunas = 4
@@ -83,7 +54,7 @@ for i in range(t):
     for j in range(colunas):
         d = i+j*t
         if d < len(TESTE):
-            print("{0:2} : {1:15}".format(d, TESTE[d][0]), end="")
+            print("{0:2}: {1:15}".format(d, TESTE[d][0]), end="")
     print()
 
 # Se um parâmetro fornecido, usar ele como escolha do exemplo
